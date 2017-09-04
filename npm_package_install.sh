3084119404647e770442adc4dfb6f03c5b3c46d9
ryuta <ryuta.hara@topgate.co.jp>
2017-09-04 12:02:34 +0900

    Change: npm list file name

---
+++ npm_package_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+npm i -g @angular/cli \
+  eslint \
+  firebase-tools \
+  git-open \
+  gitbook-cli \
+  glob \
+  js-beautify \
+  markdown-pdf \
+  npm \
+  remark-cli \
+  tern \
+  tslint \
+  tslint-react \
+  typescript \
+  typescript-formatter \
+  vimdebug
