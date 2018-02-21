95b201ed4ecf29f483fe6c9a23bbb267c1546dc4
ryutah <ryuta0221@gmail.com>
2018-02-21 22:47:47 +0900

    Update: go packages

--- go_packages.sh
+++ go_packages.sh
@@ -2,3 +2,5 @@
 go get -u github.com/motemen/ghq
 go get -u github.com/golang/dep
 go get -u github.com/motemen/gore
+go get -u github.com/golang/mock/gomock
+go get -u github.com/golang/mock/mockgen
