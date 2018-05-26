57093b0443a18ea843edfacfd60ae08fe0ea3105
ryutah <ryuta0221@gmail.com>
2018-05-26 14:53:25 +0900

    Add: go dependencies

--- go_packages.sh
+++ go_packages.sh
@@ -4,3 +4,5 @@ go get -u github.com/golang/dep
 go get -u github.com/motemen/gore
 go get -u github.com/golang/mock/gomock
 go get -u github.com/golang/mock/mockgen
+go get -u google.golang.org/grpc
+go get -u github.com/golang/protobuf/protoc-gen-go
