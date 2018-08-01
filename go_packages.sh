8ccc49e9c5a2ed96187b756fa805d55c38847edc
ryutah <ryuta0221@gmail.com>
2018-08-01 13:33:57 +0900

    Update: golang dep repository path

--- go_packages.sh
+++ go_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 go get -u github.com/motemen/ghq
-go get -u github.com/golang/dep
+go get -u github.com/golang/dep/cmd/dep
 go get -u github.com/motemen/gore
 go get -u github.com/golang/mock/gomock
 go get -u github.com/golang/mock/mockgen
